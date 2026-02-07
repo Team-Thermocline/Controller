c355725c74489678af3490aaf7c0715639b06313
KenwoodFox <joe@kitsunehosting.net>
2026-02-07 00:44:16 -0500

    Ignore build

--- Firmware/build/CMakeFiles/tcode_build_info_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tcode_build_info_h"
-  "generated/tcode_build_info.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tcode_build_info_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Firmware/build/CMakeFiles/tcode_build_info_h.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for tcode_build_info_h.
-# This may be replaced when dependencies are built.
