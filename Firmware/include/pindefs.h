b60002a88cc73db1212b40264787f47a867448ff
KenwoodFox <joe@kitsunehosting.net>
2026-02-07 01:01:57 -0500

    Include pindefs from schematic

--- Firmware/include/pindefs.h
+++ Firmware/include/pindefs.h
@@ -1,11 +1,44 @@
 #pragma once
 
+// Switches
+#define SWITCH_PIN_1 0
+#define SWITCH_PIN_2 1
+#define SWITCH_PIN_3 2
+#define SWITCH_PIN_4 3
+#define SWITCH_PIN_5 4
+#define SWITCH_PIN_6 5
+
+
 // Status LED pin
 #define STAT_LED_PIN 6
 #define FAULT_LED_PIN 7
 
+// i2c pins
+#define I2C_SDA_PIN 8
+#define I2C_SCL_PIN 9
+
 // Load Pins
 #define LOAD_PIN_1 15
+#define LOAD_PIN_2 14
+#define LOAD_PIN_3 13
+#define LOAD_PIN_4 12
+#define LOAD_PIN_5 10 // Swapped!
+#define LOAD_PIN_6 11 // Swapped!
+
+// GPIO pins
+#define GPIO_PIN_1 18
+#define GPIO_PIN_2 17
+#define GPIO_PIN_3 16
 
 // Neopixel driver
-#define NEOPIXEL_PIN 23
+#define NEOPIXEL_PIN 19
+
+// HMI
+#define HMI_AUX_PIN 20
+#define HMI_RX_PIN 21
+#define HMI_TX_PIN 22
+
+// ADC and Analog Pins
+#define ADC_TMUX_PIN 26 // ADC0 
+#define ADC_UNUSED_PIN 27 // ADC1
+#define ADC_GPIO_PIN 28 // ADC2
